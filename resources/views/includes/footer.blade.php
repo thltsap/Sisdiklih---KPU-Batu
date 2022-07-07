96c52af1b496df51221ccb001294bce763cb979b
ThalitaSaniyyaAqillaPutri <79181196+ThalitaSaniyyaAqillaPutri@users.noreply.github.com>
2022-07-07 13:55:46 +0700

    User Pages - 2

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,9 +5,9 @@
             <div class="col-lg-9 col-md-12">
                 <h4 class="text-white mb-4 ">Get In Touch</h4>
                 <h2 class="text-primary mb-6"><img src="../images/kpulogo.png" width="60px" alt="">KPU Kota Batu</h2>
-                <p class="mb-2 "><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                <p class="mb-2 "><i class="fa fa-map-marker-alt me-3"></i>Jl. Sultan Agung No.16, Sisir, Kec. Batu, Kota Batu, Jawa Timur 65314</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>(0341) 511123</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>humas@kpu-kotabatu.go.id</p>
             </div>
             {{--  <div class="col-lg-3 col-md-6">
                 <h4 class="text-light mb-4">Quick Links</h4>
@@ -28,10 +28,10 @@
             <div class="col-lg-3 col-md-6">
                 <h4 class="text-light mb-4">Follow Us</h4>  
                 <div>
-                    <a class="btn btn-square btn-outline-light me-1" href=""><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-square btn-outline-light me-1" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-square btn-outline-light me-1" href=""><i class="fab fa-youtube"></i></a>
-                    <a class="btn btn-square btn-outline-light me-0" href=""><i class="fab fa-linkedin-in"></i></a>
+                    <a class="col-lg-3 col-md-6" href="https://www.twitter.com/kpubatu"><i class="fab fa-twitter me-3"></i>@kpubatu</a><br>
+                    <a class="col-lg-3 col-md-6" href="https://www.facebook.com/officialkpubatu"><i class="fab fa-facebook-f me-3"></i>Komisi Pemilihan Umum Kota Batu</a><br>
+                    <a class="col-lg-3 col-md-6" href="https://www.youtube.com/channel/UCiDX3y0GiFxJEE8MMkBkYew"><i class="fab fa-youtube me-3"></i>KPU Kota Batu</a><br>
+                    <a class="col-lg-3 col-md-6" href="https://www.instagram.com/kpukotabatu"><i class="fab fa-instagram me-3"></i>kpukotabatu</a><br>
                 </div>
             </div>
         </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,12 +16,12 @@ KPU Batu
         <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="w-100" src="images/kpulogo.png " alt="Image">
+                    <img class="w-100" src="images/depan.jpg" alt="Image">
                     <div class="carousel-caption">
                         <div class="container">
                             <div class="row justify-content-center">
                                 <div class="col-lg-7">
-                                    <h1 class="display-2 text-light mb-5 animated slideInDown">Learn To Drive With Confidence</h1>
+                                    <h1 class="display-2 text-light mb-5 animated slideInDown">Jadilah Generasi Anti Golput</h1>
                                     <a href="" class="btn btn-primary py-sm-3 px-sm-5">Learn More</a>
                                     <a href="" class="btn btn-light py-sm-3 px-sm-5 ms-3">Our Courses</a>
                                 </div>
@@ -30,12 +30,12 @@ KPU Batu
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img class="w-100" src="img/carousel-2.jpg" alt="Image">
+                    <img class="w-100" src="images/depan2.jpeg" alt="Image">
                     <div class="carousel-caption">
                         <div class="container">
                             <div class="row justify-content-center">
                                 <div class="col-lg-7">
-                                    <h1 class="display-2 text-light mb-5 animated slideInDown">Safe Driving Is Our Top Priority</h1>
+                                    <h1 class="display-2 text-light mb-5 animated slideInDown">Pilihlah Pemimpin Negeri Ini</h1>
                                     <a href="" class="btn btn-primary py-sm-3 px-sm-5">Learn More</a>
                                     <a href="" class="btn btn-light py-sm-3 px-sm-5 ms-3">Our Courses</a>
                                 </div>
@@ -65,16 +65,20 @@ KPU Batu
             <div class="row g-5">
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="position-relative overflow-hidden ps-5 pt-5 h-100" style="min-height: 400px;">
-                        <img class="position-absolute w-100 h-100" src="images/kpulogo.png" alt="" style="object-fit: cover;">
-                        <img class="position-absolute top-0 start-0 bg-white pe-3 pb-3" src="images/kpulogo.png" alt="" style="width: 200px; height: 200px;">
+                        <div class="media">
+                            <div class="media-body">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/BYC-kZcTkFI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <img class="position-absolute top-0 start-0 pe-3 pb-3" src="images/kpulogo.png" alt="" style="width: 100px; height: 100px;">
                     </div>
                 </div>
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="h-100">
-                        <h6 class="text-primary text-uppercase mb-2">Tentang Kami</h6>
-                        <h1 class="display-6 mb-4">Komisi Pemilihan Kota Batu</h1>
-                        <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                        <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                    <div class="h-50">
+                        <h6 class="text-primary text-uppercase mb-2">Tentang Panduan</h6>
+                        <h1 class="display-6 mb-4">Panduan Pemilihan</h1>
+                        <p>Mengisi data diri pada form yang telah disediakan agar data kalian tercatat dalam daftar pemilih. Selengkapnya dapat melihat halaman panduan untuk lebih lanjut. </p>
+                        {{--  <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>  --}}
                         {{--  <div class="row g-2 mb-4 pb-2">
                             <div class="col-sm-6">
                                 <i class="fa fa-check text-primary me-2"></i>Fully Licensed
@@ -91,7 +95,7 @@ KPU Batu
                         </div>
                         <div class="row g-4">
                             <div class="col-sm-6">
-                                <a class="btn btn-primary py-3 px-5" href="">Read More</a>
+                                <a class="btn btn-primary py-3 px-3" href="">Read More</a>
                             </div>
                             {{--  <div class="col-sm-6">
                                 <a class="d-inline-flex align-items-center btn btn-outline-primary border-2 p-2" href="tel:+0123456789">
@@ -114,26 +118,28 @@ KPU Batu
     <div class="container-xxl courses my-6 py-6 pb-0">
         <div class="container">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <h6 class="text-primary text-uppercase mb-2">Tranding Courses</h6>
-                <h1 class="display-6 mb-4">Our Courses Upskill You With Driving Training</h1>
+                <h6 class="text-primary text-uppercase mb-2">Halaman Lainnya</h6>
+                <h1 class="display-6 mb-4">Pelajari Lebih Lanjut</h1>
             </div>
             <div class="row g-4 justify-content-center">
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="courses-item d-flex flex-column bg-white overflow-hidden h-100">
                         <div class="text-center p-4 pt-0">
                             <div class="fs-5 py-1 px-4 mb-4"></div>
-                            <h5 class="mb-3">Automatic Car Lessons</h5>
+                            <h5 class="mb-3">KPU</h5>
                             <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
                             {{--  <ol class="breadcrumb justify-content-center mb-0">
                                 <li class="breadcrumb-item small"><i class="fa fa-signal text-primary me-2"></i>Beginner</li>
                                 <li class="breadcrumb-item small"><i class="fa fa-calendar-alt text-primary me-2"></i>3 Week</li>
                             </ol>  --}}
                         </div>
-                        <div class="position-relative mt-auto">
-                            <img class="img-fluid" src="images/kpulogo.png" alt="">
+                        <div class="position-relative mt-3">
+                            <center>
+                            <img class="img-fluid" src="images/kpumelayani.png" alt="">
                             <div class="courses-overlay">
                                 <a class="btn btn-outline-primary border-2" href="">Read More</a>
                             </div>
+                        </center>
                         </div>
                     </div>
                 </div>
@@ -141,15 +147,15 @@ KPU Batu
                     <div class="courses-item d-flex flex-column bg-white overflow-hidden h-100">
                         <div class="text-center p-4 pt-0">
                             <div class="fs-5 py-1 px-4 mb-4"></div>
-                            <h5 class="mb-3">Highway Driving Lesson</h5>
-                            <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
+                            <h5 class="mb-3">Panduan</h5>
+                            <p>Panduan untuk menjadi pemilih yang baik dan benar</p>
                             {{--  <ol class="breadcrumb justify-content-center mb-0">
                                 <li class="breadcrumb-item small"><i class="fa fa-signal text-primary me-2"></i>Beginner</li>
                                 <li class="breadcrumb-item small"><i class="fa fa-calendar-alt text-primary me-2"></i>3 Week</li>
                             </ol>  --}}
                         </div>
-                        <div class="position-relative mt-auto">
-                            <img class="img-fluid" src="images/kpulogo.png" alt="">
+                        <div class="position-relative mt-2">
+                            <img class="img-fluid" src="images/kotak2.png" alt="">
                             <div class="courses-overlay">
                                 <a class="btn btn-outline-primary border-2" href="">Read More</a>
                             </div>
@@ -160,18 +166,20 @@ KPU Batu
                     <div class="courses-item d-flex flex-column bg-white overflow-hidden h-100">
                         <div class="text-center p-4 pt-0">
                             <div class="fs-5 py-1 px-4 mb-4"></div>
-                            <h5 class="mb-3">International Driving</h5>
-                            <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
+                            <h5 class="mb-3">Form</h5>
+                            <p>Isi form data diri untuk menjadi pemilih</p>
                             {{--  <ol class="breadcrumb justify-content-center mb-0">
                                 <li class="breadcrumb-item small"><i class="fa fa-signal text-primary me-2"></i>Beginner</li>
                                 <li class="breadcrumb-item small"><i class="fa fa-calendar-alt text-primary me-2"></i>3 Week</li>
                             </ol>  --}}
                         </div>
-                        <div class="position-relative mt-auto">
-                            <img class="img-fluid" src="images/kpulogo.png" alt="">
+                        <div class="position-relative mt-2">
+                            <center>
+                            <img class="img-fluid" src="images/kotak.png" alt="" width="300" height="300" </>
                             <div class="courses-overlay">
                                 <a class="btn btn-outline-primary border-2" href="">Read More</a>
                             </div>
+                            </center>
                         </div>
                     </div>
                 </div>               
