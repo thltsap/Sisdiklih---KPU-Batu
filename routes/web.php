d3be4becaa1f89233fc322761cd92f6dcc5aeb3f
ThalitaSaniyyaAqillaPutri <79181196+ThalitaSaniyyaAqillaPutri@users.noreply.github.com>
2022-07-08 09:41:21 +0700

    Admin Page - 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserDataController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[HomeController::class,'index'])->name('home');
 Route::get('/about',[HomeController::class,'about'])->name('about');
 Route::get('/contact',[HomeController::class,'contact'])->name('contact');
+
+Route::get('/', [DashboardController::class, 'index'])
+        ->name('dashboard');
+       
+        Route::resource('/user-data', UserDataController::class);
+        Route::resource('/contact', ContactController::class);        
